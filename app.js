e82b2616449111073a1a02bc5d88b2e096a02e03
Sureshsahu7 <85033183+Sureshsahu7@users.noreply.github.com>
2022-12-10 17:38:18 +0530

    update

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ mongoose
   .catch((err) => console.error(err));
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
   const path = require("path");
   app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
   });
 }
